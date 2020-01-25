7ce405ab316ace39da8618067c6bb767cfb30c3f
Wesley Tunnermann <wesleytunnermann2@hotmail.com>
2020-01-25 09:38:45 -0800

    Update nuget package feed location for Azure DevOps organization name change

--- packaging/push_package.bat
+++ packaging/push_package.bat
@@ -1,5 +1,5 @@
 
 echo You are about to push this package to the package server
 pause
-nuget.exe push -Source https://pkgs.dev.azure.com/Gravenger/Gravenger/_packaging/gravenger/nuget/v3/index.json -ApiKey key Gravenger.Domain.0.1.0.nupkg
+nuget.exe push -Source https://pkgs.dev.azure.com/Seenry/Gravenger/_packaging/gravenger/nuget/v3/index.json -ApiKey key Gravenger.Domain.0.1.0.nupkg
 pause
